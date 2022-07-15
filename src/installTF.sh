412d3db4476e19ba2f394ad564e031cb98d80889
Carl Alleyne <alleynec@gmail.com>
2022-07-15 10:43:30 -0400

    modified component commands

--- src/installTF.sh
+++ src/installTF.sh
@@ -6,8 +6,8 @@ function installTF {
     curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${INPUT_TERRAFORMVERSION}/terraform_${INPUT_TERRAFORMVERSION}_linux_amd64.zip
     echo -e "${INPUT_TERRAFORMSHA256} terraform.zip" | sha256sum -c -s
     unzip terraform.zip
-    mv terraform /usr/bin
-    cat > /root/.terraform.rc << EOF
+    sudo mv terraform /usr/bin
+    sudo cat > /root/.terraform.rc << EOF
 credentials "app.terraform.io" {
     token = "${INPUT_TERRAFORMCLOUDTOKEN}"
 }
