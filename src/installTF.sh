7e65dc8bcc3e213db37ab0919b54c7688fdfea18
alleynec <42039158+alleynec@users.noreply.github.com>
2022-07-19 14:29:52 -0400

    Fix sudos (#15)
    * correct use of sudo
    
    * correctly creating TF folder

--- src/installTF.sh
+++ src/installTF.sh
@@ -6,7 +6,7 @@ function installTF {
     curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${INPUT_TERRAFORMVERSION}/terraform_${INPUT_TERRAFORMVERSION}_linux_amd64.zip
     echo -e "${INPUT_TERRAFORMSHA256} terraform.zip" | sha256sum -c -s
     unzip terraform.zip
-    mkdir ~/bin
+    mkdir -p ~/bin
     mv terraform ~/bin
     cat > /root/.terraform.rc << EOF
 credentials "app.terraform.io" {
